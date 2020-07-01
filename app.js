673dea43eddd07efe3de9b718a7298a4f9783f75
Jack Carney <c3279042@uon.edu.au>
2020-07-01 15:54:46 +1000

    testing query strings

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get("/route1", (req, res) => {
+	let query1 = req.query["one"]
+	let query2 = req.query["two"]
+
+	res.status(200).send(`got one as ${query1} and got two as ${query2}`)
+
+})
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
